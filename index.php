23b0ce063cf9aed93894a7458d96c5247bfaad95
tov_groliv <112928024+tovgroliv@users.noreply.github.com>
2023-03-10 18:43:58 +0300

    Обновлен index.php
    - Отображает рендер класса User

--- index.php
+++ index.php
@@ -1,8 +1,14 @@
+<? require_once($_SERVER['DOCUMENT_ROOT'] . "/modules/user.php") ?>
+
 <html>
     <head>
         <title>ValueConverter</title>
     </head>
     <body>
-        
+        <?php
+            $user = new User();
+
+            $user->render();
+        ?>
     </body>
 </html>
