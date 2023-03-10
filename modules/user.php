656ddb590a442b41c8901213b6cc451936d4f268
tov_groliv <112928024+tovgroliv@users.noreply.github.com>
2023-03-10 18:49:18 +0300

    Разделена логика отрисовки личного кабинета

--- modules/user.php
+++ modules/user.php
@@ -56,48 +56,15 @@ class User
     }
     private function renderUserControlPanel()
     {
-        $select = "";
-        $values = ValueConverter::getAllValue();
-        foreach ($values as $key => $value) {
-            $selected = $_GET["valueId"] === $key ? "selected" : "";
-            $select .= "<option value='{$key}' {$selected}>{$value->name}</option>";
-        }
-
         $welcome = "Welcome, {$this->login} #{$this->id}!";
+        echo "<h1>{$welcome}</h1>";
+        echo ValueConverter::render();
+        $this->renderLogout();
+    }
 
-        $valueTo = 0;
-        $valueFrom = 0;
-
-        if (isset($_GET["valueTo"]) && isset($_GET["valueId"]))
-        {
-            $valueTo = $values[$_GET["valueId"]]->valueTo($_GET["valueTo"]);
-        }
-        if (isset($_GET["valueFrom"]) && isset($_GET["valueId"]))
-        {
-            $valueFrom = $values[$_GET["valueId"]]->valueFrom($_GET["valueFrom"]);
-        }
-
+    private function renderLogout()
+    {
         echo "
-            <h1>{$welcome}</h1>
-            <form method='get'>
-                <p>
-                    <label>Рублей в валюте</label>
-                    <input name='valueFrom' type='number' value='{$_GET["valueFrom"]}' />
-                    <label>{$valueFrom}</label>
-                </p>
-                <p>
-                    <label>Валютсы в рублях </label>
-                    <input name='valueTo' type='number' value='{$_GET["valueTo"]}' />
-                    <label>{$valueTo}</label>
-                </p>
-                <p>
-                    <label>Валюта для конвертации</label>
-                    <select name='valueId'>{$select}</select>
-                </p>
-                <p>
-                    <input type='submit' value='Перевести' />
-                </p>
-            </form>
             <form method='post' action='services/post/logout.php'>
                 <input type='submit' value='Выход' />
             </form>
--- modules/value_converter.php
+++ modules/value_converter.php
@@ -22,6 +22,51 @@ class ValueConverter
         $this->value = $value;
     }
 
+    public static function render()
+    {
+        $select = "";
+        $values = ValueConverter::getAllValue();
+        foreach ($values as $key => $value) {
+            $selected = $_GET["valueId"] === $key ? "selected" : "";
+            $select .= "<option value='{$key}' {$selected}>{$value->name}</option>";
+        }
+
+
+        $valueTo = 0;
+        $valueFrom = 0;
+
+        if (isset($_GET["valueTo"]) && isset($_GET["valueId"]))
+        {
+            $valueTo = $values[$_GET["valueId"]]->valueTo($_GET["valueTo"]);
+        }
+        if (isset($_GET["valueFrom"]) && isset($_GET["valueId"]))
+        {
+            $valueFrom = $values[$_GET["valueId"]]->valueFrom($_GET["valueFrom"]);
+        }
+
+        echo "
+            <form method='get'>
+                <p>
+                    <label>Рублей в валюте</label>
+                    <input name='valueFrom' type='number' value='{$_GET["valueFrom"]}' />
+                    <label>{$valueFrom}</label>
+                </p>
+                <p>
+                    <label>Валютсы в рублях </label>
+                    <input name='valueTo' type='number' value='{$_GET["valueTo"]}' />
+                    <label>{$valueTo}</label>
+                </p>
+                <p>
+                    <label>Валюта для конвертации</label>
+                    <select name='valueId'>{$select}</select>
+                </p>
+                <p>
+                    <input type='submit' value='Перевести' />
+                </p>
+            </form>
+        ";
+    }
+
     public static function getAllValue()
     {
         $result = DataBase::getRequest("SELECT * FROM `Value`");
@@ -72,6 +117,10 @@ class ValueConverter
 
     public function valueTo($amount)
     {
+        if ($amount == 0)
+        {
+            return 0;
+        }
         return $amount * $this->nominal / $this->value;
     }
 }
