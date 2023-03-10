3449459d15c5af790eb33870aebcd98800340ed9
tov_groliv <112928024+tovgroliv@users.noreply.github.com>
2023-03-10 18:38:47 +0300

    Обновлен код session.php
    - Добавлена очиста сессии
    - Добавлена установка сессии

--- services/sessions.php
+++ services/sessions.php
@@ -7,6 +7,16 @@ class Sessions
         session_start();
     }
 
+    public static function setSession($name, $value)
+    {
+        if (session_status() === PHP_SESSION_NONE)
+        {
+            session_start();
+        }
+        
+        $_SESSION[$name] = $value;
+    }
+
     public static function getSession($name)
     {
         if (session_status() === PHP_SESSION_NONE)
@@ -15,5 +25,15 @@ class Sessions
         }
         
         return $_SESSION[$name];
+    }
+
+    public static function unsetSession($name)
+    {
+        if (session_status() === PHP_SESSION_NONE)
+        {
+            session_start();
+        }
+        
+        unset($_SESSION[$name]);
     }
 }
